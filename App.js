093acc91db717b7cdf15f97f1c4b011c64bab7e8
okarslan <okarslan@dabnet.com.tr>
2024-04-21 23:09:49 +0300

    create blogscreen

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import IndexScreen from './screens/IndexScreen';
-import CreateScreen from './screens/CreateScreen';
-import {  Provider } from './context/BlogContext';
-import ShowScreen from './screens/ShowScreen';
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import IndexScreen from "./screens/IndexScreen";
+import CreateScreen from "./screens/CreateScreen";
+import { Provider } from "./context/BlogContext";
+import ShowScreen from "./screens/ShowScreen";
+import { AntDesign } from "@expo/vector-icons";
+import EditScreen from "./screens/EditScreen";
+import { EvilIcons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Provider>
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerTitle:'Blog App'}}>
-        <Stack.Screen name="Index" component={IndexScreen} />
-        <Stack.Screen name="Create" component={CreateScreen} />
-        <Stack.Screen name="Show" component={ShowScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerTitle: "Blog App" }}>
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                  <AntDesign name="plus" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen name="Create" component={CreateScreen} />
+          <Stack.Screen name="Show" component={ShowScreen}  options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+                  <EvilIcons name="pencil" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })} />
+          <Stack.Screen name="Edit" component={EditScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    
-  },
+  container: {},
 });
