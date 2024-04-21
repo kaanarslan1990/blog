c3735c0a477f4ab9c34274f5f383d2a260d491bb
okarslan <okarslan@dabnet.com.tr>
2024-04-21 16:29:57 +0300

    useContext added

---
+++ context/BlogContext.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([
+    { title: "React Native" },
+    { title: "JavaScript" },
+  ]);
+  const addBlogPost =() => {
+    setBlogPosts([...blogPosts,{title:'Vue JS'}])
+  }
+  return (
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useContext } from "react";
+import BlogContext from "../context/BlogContext";
 
 export default function IndexScreen() {
+  const {data,addBlogPost} = useContext(BlogContext);
   return (
     <View>
-      <Text>IndexScreen</Text>
+      {/* <Text>IndexScreen</Text> */}
+      <Button title="Add" onPress={addBlogPost}/>
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
