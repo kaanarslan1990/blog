5e3a7f3812692ebaffa97891c6ef1fd91bbcc66e
okarslan <okarslan@dabnet.com.tr>
2024-04-21 16:38:03 +0300

    usereducer

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 
 const BlogContext = React.createContext();
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: "Angular" }];
+    default:
+      return state;
+  }
+};
 
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([
+  const [blogPosts, dispatch] = useReducer(blogReducer, [
     { title: "React Native" },
     { title: "JavaScript" },
   ]);
-  const addBlogPost =() => {
-    setBlogPosts([...blogPosts,{title:'Vue JS'}])
-  }
+  // const [blogPosts, setBlogPosts] = useState([
+  //   { title: "React Native" },
+  //   { title: "JavaScript" },
+  // ]);
+  const addBlogPost = () => {
+    dispatch({ type: "add_blogpost" });
+  };
   return (
     <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
       {children}
